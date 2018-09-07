08c52f747d21c996acd4bf816afb329177fb4702
victor.petukhov <victor.petukhov@jetbrains.com>
2018-09-07 20:12:44 +0300

    Add doc to ExperimentalContracts

--- libraries/stdlib/src/kotlin/contracts/ContractBuilder.kt
+++ libraries/stdlib/src/kotlin/contracts/ContractBuilder.kt
@@ -8,31 +8,123 @@ package kotlin.contracts
 import kotlin.internal.ContractsDsl
 import kotlin.internal.InlineOnly
 
+/**
+ * Signals that the annotated annotation class is a marker of an experimental contracts API.
+ * Any declaration annotated with that marker can only be used with the compiler argument `-Xuse-experimental=kotlin.contracts.ExperimentalContracts`.
+ */
 @Retention(AnnotationRetention.BINARY)
 @SinceKotlin("1.3")
 @Experimental
 public annotation class ExperimentalContracts
 
+/**
+ * This DSL used to specify effects of a contract.
+ * Effect is a piece of knowledge about program context, it can be expression some function's behavior.
+ * Contract DSL can be used only in the top-level functions.
+ */
 @ContractsDsl
 @ExperimentalContracts
 @SinceKotlin("1.3")
 public interface ContractBuilder {
+    /**
+     * Expresses that function returned successfully.
+     *
+     * @sample samples.contracts.returnsContract
+     * @sample samples.contracts.Contracts.Smartcasts.continueWithNotNull
+     */
     @ContractsDsl public fun returns(): Returns
+
+    /**
+     * Expresses that function returned with some value.
+     * Value can only be `true`, `false` or `null`.
+     *
+     * @sample samples.contracts.returnsTrueContract
+     * @sample samples.contracts.returnsFalseContract
+     * @sample samples.contracts.returnsNullContract
+     *
+     * @sample samples.contracts.Contracts.Smartcasts.continueWithInt
+     * @sample samples.contracts.Contracts.Smartcasts.continueWithNotNullString
+     * @sample samples.contracts.Contracts.Smartcasts.continueWithNotNullInt
+     */
     @ContractsDsl public fun returns(value: Any?): Returns
+
+    /**
+     * Expresses that function returned with some not null value.
+     *
+     * @sample samples.contracts.returnsNotNullContract
+     * @sample samples.contracts.Contracts.Smartcasts.continueWithNull
+     */
     @ContractsDsl public fun returnsNotNull(): ReturnsNotNull
+
+    /**
+     * Expresses that:
+     *  1) [lambda] will not be called after the call to owner-function is finished;
+     *  2) [lambda] will not be passed to another function without the similar contract.
+     * [kind] indicates that [lambda] is guaranteed to be invoked some statically known amount of times.
+     *
+     * Note that a function with the `callsInPlace` effect must be inline.
+     *
+     * @sample samples.contracts.Contracts.Initialization
+     */
     @ContractsDsl public fun <R> callsInPlace(lambda: Function<R>, kind: InvocationKind = InvocationKind.UNKNOWN): CallsInPlace
 }
 
+/**
+ * `InvocationKind` enum class is used to specify the amount of times, that `callable` which is used in the `callsInPlace` effect guaranteed will be invoked.
+ */
 @ContractsDsl
 @ExperimentalContracts
 @SinceKotlin("1.3")
 public enum class InvocationKind {
+    /**
+     * Means that the `callable` will be invoked zero or one time.
+     *
+     * @sample samples.contracts.callsInPlaceAtMostOnceContract
+     * @sample samples.contracts.Contracts.Initialization.valPossibleInitialization
+     */
     @ContractsDsl AT_MOST_ONCE,
+
+    /**
+     * Means that the `callable` will be invoked one or more times.
+     *
+     * @sample samples.contracts.callsInPlaceAtLeastOnceContract
+     * @sample samples.contracts.Contracts.Initialization.varInitializationOrReassignment
+     */
     @ContractsDsl AT_LEAST_ONCE,
+
+    /**
+     * Means that the `callable` will be invoked exactly one time.
+     *
+     * @sample samples.contracts.callsInPlaceExactlyOnceContract
+     * @sample samples.contracts.Contracts.Initialization.valInitialization
+     */
     @ContractsDsl EXACTLY_ONCE,
+
+    /**
+     * Means that the `callable` will be invoked unknown amount of times.
+     *
+     * @sample samples.contracts.callsInPlaceUnknownContract
+     * @sample samples.contracts.Contracts.Initialization.varPossibleInitialization
+     */
     @ContractsDsl UNKNOWN
 }
 
+/**
+ * The function to describe a contract.
+ * The contract description must be at the beginning of a function and has at least one effect.
+ *
+ * @param builder the lambda in the body of which the effects from the `ContractBuilder` are specified.
+ *
+ * @sample samples.contracts.returnsContract
+ * @sample samples.contracts.returnsTrueContract
+ * @sample samples.contracts.returnsFalseContract
+ * @sample samples.contracts.returnsNullContract
+ * @sample samples.contracts.returnsNotNullContract
+ * @sample samples.contracts.callsInPlaceAtMostOnceContract
+ * @sample samples.contracts.callsInPlaceAtLeastOnceContract
+ * @sample samples.contracts.callsInPlaceExactlyOnceContract
+ * @sample samples.contracts.callsInPlaceUnknownContract
+ */
 @ContractsDsl
 @ExperimentalContracts
 @InlineOnly
--- libraries/stdlib/src/kotlin/contracts/Effect.kt
+++ libraries/stdlib/src/kotlin/contracts/Effect.kt
@@ -12,31 +12,63 @@ import kotlin.internal.ContractsDsl
 @SinceKotlin("1.3")
 public interface Effect
 
+/**
+ * `ConditionalEffect` corresponds to an additional condition applied to the [SimpleEffect].
+ * `ConditionalEffect` can only be used in conjunction with [SimpleEffect].
+ *
+ * @see SimpleEffect.implies
+ */
 @ContractsDsl
 @ExperimentalContracts
 @SinceKotlin("1.3")
 public interface ConditionalEffect : Effect
 
+/**
+ * `SimpleEffect` describes of function's behavior regardless of any conditions.
+ */
 @ContractsDsl
 @ExperimentalContracts
 @SinceKotlin("1.3")
 public interface SimpleEffect {
+    /**
+     * `implies` allows to specify an additional condition for some `SimpleEffect`.
+     *
+     * @param booleanExpression the additional condition which is subset of Kotlin boolean expressions.
+     *
+     * Note: subset of Kotlin boolean expressions is true/false/null checks for function parameter or receiver, also allowed conjunction and disjunction.
+     */
     @ContractsDsl
     @ExperimentalContracts
     public infix fun implies(booleanExpression: Boolean): ConditionalEffect
 }
 
-
+/**
+ * `Returns` effect describes the return value of some function.
+ * The return value can only be `true`, `false` or `null`.
+ *
+ * @see ContractBuilder.returns
+ */
 @ContractsDsl
 @ExperimentalContracts
 @SinceKotlin("1.3")
 public interface Returns : SimpleEffect
 
+/**
+ * `ReturnsNotNull` effect describes that the return value of some function is not null.
+ * The return value can only be `true`, `false` or `null`.
+ *
+ * @see ContractBuilder.returnsNotNull
+ */
 @ContractsDsl
 @ExperimentalContracts
 @SinceKotlin("1.3")
 public interface ReturnsNotNull : SimpleEffect
 
+/**
+ * `CallsInPlace` effect describes of function's behavior in relation to the invocation of the received lambda.
+ *
+ * @see ContractBuilder.callsInPlace
+ */
 @ContractsDsl
 @ExperimentalContracts
 @SinceKotlin("1.3")
